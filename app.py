ca7f4514afc16ab01ea2bd4906db1908da33c7cb
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-27 06:40:11 +0530

    Refactor

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
 from datetime import datetime
+
+from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def homepage():
@@ -13,6 +16,6 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
