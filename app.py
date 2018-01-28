5a1a72aa966055c2a61be4ddf15333632b3405fb
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-28 10:45:37 +0530

    Added facebook and instagram routes

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/facebook_login')
+def facebook_login():
+    return render_template('facebook_login.html', app_id='101206834030831')
+
+
+@app.route('/instagram_login')
+def facebook_login():
+    return render_template('instagram_login.html', app_id='101206834030831')
+
+
 @app.route('/')
 def homepage():
     return render_template('home.html', app_id='101206834030831')
