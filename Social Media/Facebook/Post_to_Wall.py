e58cb7ff69f50a4fe5834b54ababd633a0413183
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-28 18:24:25 +0530

    Posting to entered user wall working

--- Social Media/Facebook/Post_to_Wall.py
+++ Social Media/Facebook/Post_to_Wall.py
@@ -2,17 +2,17 @@ import facebook
 
 
 def main():
-    graph = facebook.GraphAPI(access_token='your_user_access_token')
+    graph = facebook.GraphAPI(access_token='EAABcDA1kKO8BAPv5QHb3F3erAjZCZB8PjTQ1uDyjo0fTFELyhpzjTTv4Acczoyh7YdBJr1HFcqecAkK9pfUAy6lCFhWZAJibhoUUdIZCnCBOOKWxlt4KRoA7va8RiJ0m25qsmCKnyy9lMYmgU1FjCuMAL8USRpzH2Y8uXnxjc8ZBEhM90EyZBIZBKxACpLKzrsAKjmJZCksQjwZDZD')
     # if version 2.8 show error use 2.6
     attachment = {
         'name': 'Link name',
         'link': 'https://www.example.com/',
         'caption': 'Check out this example',
         'description': 'This is a longer description of the attachment',
-        'picture': 'https://www.example.com/thumbnail.jpg'
+        # 'picture': 'https://www.example.com/thumbnail.jpg'
     }
 
-    graph.put_wall_post(message='Check this out...', profile_id='your_page_id')
+    graph.put_wall_post(message='Check this out...', profile_id='326213351200598')
     # graph.put_wall_post(message='Check this out...', attachment=attachment, profile_id='your_page_id')
 
 
