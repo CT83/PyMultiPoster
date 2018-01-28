841d04740f090078653493bd79684f081505d459
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-28 09:18:19 +0530

    Facebook Authorization working!!!!

--- Social Media/Facebook/OAuth_Code_Getter.py
+++ Social Media/Facebook/OAuth_Code_Getter.py
@@ -14,3 +14,5 @@ def get_fb_token(app_id, app_secret):
 
 print(get_fb_token("162435237720033", "cd7e9f7e2d4655bc17211a5c02334d3a"))
 # https://www.facebook.com/v2.11/dialog/oauth?client_id={162435237720033}&redirect_uri={"https://pyreserver.herokuapp.com/database_admin_s"}&state={"{st=state123abc,ds=123456789}"}
+# https://www.facebook.com/v2.11/dialog/oauth?client_id={162435237720033}&redirect_uri={"https://www.getpostman.com/oauth2/callback"}&state={"{st=state123abc,ds=123456789}"}
+# https://www.facebook.com/v2.11/dialog/oauth?client_id={101206834030831}&redirect_uri={"http://localhost/"}&state={"{st=state123abc,ds=123456789}"}
--- app.py
+++ app.py
@@ -9,13 +9,62 @@ app = Flask(__name__)
 def homepage():
     the_time = datetime.now().strftime("%h")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    return """         <div id="fb-root"></div>
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+        <script>
+        window.fbAsyncInit = function() {
+                FB.init({
+                appId: '101206834030831',
+                status: true,
+                cookie: true,
+                xfbml: true
+            });
+        };
 
+        // Load the SDK asynchronously
+        (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "https://connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+        }(document));
 
+        function login() {
+            FB.login(function(response) {
+
+            // handle the response
+            console.log("Response goes here!");
+            if (response.authResponse) {
+             var access_token =   FB.getAuthResponse()['accessToken'];
+             console.log('Access Token = '+ access_token);
+             FB.api('/me', function(response) {
+             console.log('Good to see you, ' + response.name + '.');
+             });
+           } else {
+             console.log('User cancelled login or did not fully authorize.');
+           }
+
+            }, {scope: 'publish_actions'});            
+        }
+
+        function logout() {
+            FB.logout(function(response) {
+              // user is now logged out
+            });
+        }
+
+        var status = FB.getLoginStatus();
+
+        console.log(status);
+
+        </script>
+
+        <button onclick="javascript:login();">Login Facebook</button>
+
+        <br>
+
+        <button onclick="javascript:logout();">Logout from Facebook</button>
+    """
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
