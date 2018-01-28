650a15a94a95ca67b804aecec80e4f37da929ff1
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-28 11:13:46 +0530

    Added Post to instagram

--- Social Media/Instagram/instagram_post.py
+++ Social Media/Instagram/instagram_post.py
@@ -1,8 +1,8 @@
-from InstagramAPI import InstagramAPI
+def post_to_instagram():
+    from InstagramAPI import InstagramAPI
+    InstagramAPI = InstagramAPI("pyreserver.pseudtech@gmail.com", "cybertech83")
+    InstagramAPI.login()  # login
 
-InstagramAPI = InstagramAPI("pyreserver.pseudtech@gmail.com", "cybertech83")
-InstagramAPI.login()  # login
-
-photo_path = 'temp.jpg'
-caption = "Sample photo"
-InstagramAPI.uploadPhoto(photo_path, caption=caption)
+    photo_path = 'temp.jpg'
+    caption = "Sample photo"
+    InstagramAPI.uploadPhoto(photo_path, caption=caption)
--- app.py
+++ app.py
@@ -8,8 +8,19 @@ def facebook_login():
     return render_template('facebook_login.html', app_id='101206834030831')
 
 
+def post_to_instagram():
+    from InstagramAPI import InstagramAPI
+    InstagramAPI = InstagramAPI("pyreserver.pseudtech@gmail.com", "cybertech83")
+    InstagramAPI.login()  # login
+
+    photo_path = 'temp.jpg'
+    caption = "Sample photo"
+    InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+
 @app.route('/instagram_login')
 def instagram_login():
+    post_to_instagram()
     return render_template('instagram_login.html', app_id='101206834030831')
 
 
