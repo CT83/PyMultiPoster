c585b9248131c61c74fa9d9410aa00ba9ee88ca6
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-28 10:25:25 +0530

    Fixed templates link

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('home.tpl', app_id='101206834030831')
+    return render_template('home.html', app_id='101206834030831')
 
 
 if __name__ == '__main__':
