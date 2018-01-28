90516cacfb6ee353b632b47912614b581544fba5
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-28 11:10:48 +0530

    Added Post to instagram

---
+++ Social Media/Instagram/instagram_post.py
@@ -0,0 +1,8 @@
+from InstagramAPI import InstagramAPI
+
+InstagramAPI = InstagramAPI("pyreserver.pseudtech@gmail.com", "cybertech83")
+InstagramAPI.login()  # login
+
+photo_path = 'temp.jpg'
+caption = "Sample photo"
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
