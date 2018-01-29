685888830e9f068be466d858c3ca7726c38a29a2
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-29 18:15:31 +0530

    Sending tweets from user account successful

--- Social Media/Twitter/twitter_get_consent.py
+++ Social Media/Twitter/twitter_get_consent.py
@@ -5,12 +5,7 @@ CONSUMER_SECRET = 'xM7G3WocNnSYRCsIsJw7yeRDasuJ3QzxdRlS7iLZoVr92gKtAg'
 ACCESS_TOKEN = '957098797791768576-iBgwacSqxbsFM3cah3fndBjndLm8eOO'
 ACCESS_TOKEN_SECRET = '7VNkIwputPvlhWhBnltCJu9Ct9INAN5vOfgtls00YtF8x'
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
-status = "Testi1ng!2"
-api.update_status(status=status)
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 try:
     redirect_url = auth.get_authorization_url()
@@ -18,3 +13,17 @@ try:
 except tweepy.TweepError:
     print('Error! Failed to get request token.')
 print(auth.request_token)
+token = auth.request_token
+
+# Let's say this is a web app, so we need to re-build the auth handler
+# first...
+verifier = input('Verifier:')
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.request_token = token
+try:
+    auth.get_access_token(verifier)
+    print('Got access!!')
+    api = tweepy.API(auth)
+    api.update_status(status="Sent fomre ")
+except tweepy.TweepError:
+    print('Error! Failed to get access token.')
