72113fa5f7a04a837d1bb4da9d0a7ecd617ec9ff
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-30 19:36:47 +0530

    Changed requirements.txt

--- Social Media/LinkedIn/linkedin_post_share.py
+++ Social Media/LinkedIn/linkedin_post_share.py
@@ -2,10 +2,29 @@ from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication,
                                PERMISSIONS)
 
 if __name__ == '__main__':
-    API_KEY = 'wFNJekVpDCJtRPFX812pQsJee-gt0zO4X5XmG6wcfSOSlLocxodAXNMbl0_hw3Vl'
-    API_SECRET = 'daJDa6_8UcnGMw1yuq9TjoO_PMKukXMo8vEMo7Qv5J-G3SPgrAV0FqFCd0TNjQyG'
-    RETURN_URL = 'http://localhost:8000'
-    authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL,
-                                            PERMISSIONS.enums.values())
+
+    CLIENT_ID = '<Your Client ID>'
+    CLIENT_SECRET = '<Your Client secret>'
+    RETURN_URL = 'http://localhost:8080/code/'
+
+    authentication = LinkedInAuthentication(
+                        CLIENT_ID,
+                        CLIENT_SECRET,
+                        RETURN_URL,
+                        permissions=['r_basicprofile',
+                                     'r_emailaddress',
+                                     'rw_company_admin',
+                                     'w_share']
+                    )
+
+    # Note: edit permissions according to what you defined in the linkedin
+    # developer console.
+
+    # Optionally one can send custom "state" value that will be returned from
+    # OAuth server It can be used to track your user state or something else
+    # (it's up to you) Be aware that this value is sent to OAuth server AS IS -
+    # make sure to encode or hash it
+    # authorization.state = 'your_encoded_message'
+
     print(authentication.authorization_url)
     application = LinkedInApplication(authentication)
