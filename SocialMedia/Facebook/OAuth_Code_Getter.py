787e9b3249e0fd7a63ec5a281c631aefeb1464cc
Rohan Sawant <rohansawantct83@gmail.com>
2018-01-31 19:25:46 +0530

    Reformat

--- SocialMedia/Facebook/OAuth_Code_Getter.py
+++ SocialMedia/Facebook/OAuth_Code_Getter.py
@@ -10,7 +10,6 @@ def get_fb_token(app_id, app_secret):
     # print file.text #to test the TOKEN
     return file.text
 
-
     print(get_fb_token("101206834030831", "9be8d03bb48f86245d2bad7269831f51"))
 # https://www.facebook.com/v2.11/dialog/oauth?client_id={162435237720033}&redirect_uri={"https://pyreserver.herokuapp.com/database_admin_s"}&state={"{st=state123abc,ds=123456789}"}
 # https://www.facebook.com/v2.11/dialog/oauth?client_id={162435237720033}&redirect_uri={"https://www.getpostman.com/oauth2/callback"}&state={"{st=state123abc,ds=123456789}"}
