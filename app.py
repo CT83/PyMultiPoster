8af224c79f3a7bacb5f821f96ca993711168454f
CT83 <rohansawantct83@gmail.com>
2018-02-09 04:09:01 +0530

    Removed Duplicate app initialisations

--- app.py
+++ app.py
@@ -61,7 +61,6 @@ def google91e934bee0a01da8():
 
 
 # if __name__ == '__main__':
-app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 # Models have to be imported to this file before 'db.init_app(app)'
