a076e7dd04842a510c16998b0b0a7d6fea125ecb
Rohan Sawant <rohansawantct83@gmail.com>
2018-02-10 12:59:40 +0530

    Added Printlines and refactor

--- Forms/MainPostForm.py
+++ Forms/MainPostForm.py
@@ -1,13 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, FileField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, FileField, SubmitField, TextAreaField, SelectMultipleField
+from wtforms import widgets, Form
 from wtforms.validators import InputRequired, Length, Email
 
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
 class MainPostForm(FlaskForm):
-    title = StringField('title', validators=[Length(min=6, max=20)])
-    description = StringField('description', validators=[InputRequired(), Length(min=4, max=80)])
-    photo = FileField('Your photo')
-    submit = SubmitField('Submit')
+    title = StringField('Title', validators=[Length(min=6, max=20)])
+    post = TextAreaField('Post', render_kw={"rows": 10, "cols": 70},
+                         validators=[InputRequired(), Length(min=4, max=80)])
+    photo = FileField('Photo (Optional)')
+
+    string_of_files = ['FaceBook\r\nInstagram\r\nTumblr\r\n']
+    list_of_files = string_of_files[0].split()
+    # create a list of value/description tuples
+    files = [(x, x) for x in list_of_files]
+    example = MultiCheckboxField('Social Networks', choices=files)
+    submit = SubmitField('Proceed')
 
 
 class RegisterForm(FlaskForm):
--- mainPost.py
+++ mainPost.py
@@ -11,15 +11,22 @@ app.config['WTF_CSRF_SECRET_KEY'] = "powerful secretkey"
 bootstrap = Bootstrap(app)
 
 
-@app.route('/upload', methods=('GET', 'POST'))
+@app.route('/main', methods=('GET', 'POST'))
 def upload():
     form = MainPostForm()
     if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
         filename = secure_filename(form.photo.data.filename)
         form.photo.data.save("tmp/" + filename)
+
+        print("Title:", title)
+        print("Post:", post)
+        print("Image:", filename)
+
     else:
         filename = None
-    return render_template('post/upload.html', form=form, filename=filename)
+    return render_template('post/main.html', form=form, filename=filename)
 
 
 if __name__ == '__main__':
