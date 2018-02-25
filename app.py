c82cca0d7255daf3f5e3a099467458dacb9f9e9e
Rohan Sawant <rohansawantct83@gmail.com>
2018-02-25 15:48:34 +0530

    One to many relationship working

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ class User(db.Model):
     email = db.Column(db.String(80), primary_key=True, unique=True)
     password = db.Column(db.String(80))
     name = db.Column(db.Text)
-    articles = db.relationship('posts', backref='user')
+    articles = db.relationship('Post', backref='user')
 
     def __init__(self, email, password, name):
         self.email = email
