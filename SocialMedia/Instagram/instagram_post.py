d00e9d1ed6c9595f2ad3990955dd179359da5cda
Rohan Sawant <rohansawantct83@gmail.com>
2018-03-18 08:27:05 +0530

    Removed All unecessary files from Social Media package

--- SocialMedia/Instagram/instagram_post.py
+++
@@ -1,8 +0,0 @@
-def post_to_instagram():
-    from InstagramAPI import InstagramAPI
-    InstagramAPI = InstagramAPI("pyreserver.pseudtech@gmail.com", "cybertech83")
-    InstagramAPI.login()  # login
-
-    photo_path = 'temp.jpg'
-    caption = "Sample photo"
-    InstagramAPI.uploadPhoto(photo_path, caption=caption)
