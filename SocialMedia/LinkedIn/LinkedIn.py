fbb39881db88a2c79f3239275374fe2a926f5a8e
Rohan Sawant <rohansawantct83@gmail.com>
2018-03-27 05:32:22 +0530

    Linkedin Company page posting working

--- SocialMedia/LinkedIn/LinkedIn.py
+++ SocialMedia/LinkedIn/LinkedIn.py
@@ -1,6 +1,6 @@
 from linkedin.linkedin import LinkedInApplication, LinkedInAuthentication
 
-from CONSTANT import LINKEDIN_CLIENT_SECRET, LINKEDIN_CLIENT_ID, LINKEDIN_RETURN_URL, IMGUR_CLIENT_ID
+from CONSTANT import IMGUR_CLIENT_ID, LINKEDIN_CLIENT_ID, LINKEDIN_CLIENT_SECRET
 from Imgur.Imgur import upload_to_imgur
 from SocialMedia.SocialMedia import SocialMedia
 
@@ -41,6 +41,39 @@ class LinkedInAuth:
         linkedin_auth.authorization_code = v_code
         result = linkedin_auth.get_access_token()
         return result.access_token
+
+
+def publish_update_company_page(message="", title="", company_id=""):
+    import requests
+    auth = "AQVwWpDvP_2FTR5hvTHRXNbMKlWh5sK9BDVJmKG_fzAnKKgoT6bLDrZy0z3kN-RQa2e36evsfTMMlw4gq5BwlQiBbWUq7JIZAxALCssZ4nPPdeKFlEfnehh8Qqw87_eu6-ulyVPdWEsgS-zAxpNQPRW5IlJ2tQ56Lk_jGMlnnVowikarEfCs4VIqBo1GxP8YouYpefmwK_Yebelp-VFeRCpNJ3Nx1dAeLn3d5cRNyXE8nrvDYfqxSSQrtWewiNz1u9QUS-2wmOrLXjfJSfUuOM-stOi4Cbzcnrsh9xljamU9SKj-WjGQLFjfFEMqY-K3_-4aEAODYdzto-3rIAAzV6i6YInb4w"
+
+    json_d = {
+        "visibility": {"code": "anyone"},
+        "comment": "Testing a full company share!",
+        "content": {
+            "submitted-­url": "https://www.example.com/content.html",
+            "title": "Test Share with Content",
+            "description": "content description",
+            "submitted‐image-­url": "https://www.example.com/img.jpg"
+        }
+    }
+    from flask import json
+    json_j = json.dumps(json_d)
+
+    url = "https://api.linkedin.com/v1/companies/{}" \
+          "/shares?oauth2_access_token={}" \
+          "&format=json".format(company_id, auth)
+    header = {'Content-Type': 'application/json',
+              "x-li-format": "json",
+              'Connection': 'Keep-Alive'
+              }
+    print("Headers:", header)
+    print("url:", url)
+    print("json_j:", json_j)
+
+    status = requests.post(url, headers=header, data=json_j)
+    print(str(status))
+    status = dict(status.json())
 
 
 class LinkedIn(SocialMedia):
@@ -90,18 +123,20 @@ class LinkedIn(SocialMedia):
 
 
 def main():
-    linkedin_auth = LinkedInAuth(LINKEDIN_CLIENT_ID,
-                                 LINKEDIN_CLIENT_SECRET,
-                                 LINKEDIN_RETURN_URL)
-
-    print(linkedin_auth.get_authorization_url())
-    link = input("Enter Link:")
-    auth_token = linkedin_auth.get_access_token_from_url(link)
-
-    linkedin_poster = LinkedIn(LINKEDIN_CLIENT_ID, LINKEDIN_CLIENT_SECRET, auth_token)
+    # linkedin_auth = LinkedInAuth(LINKEDIN_CLIENT_ID,
+    #                              LINKEDIN_CLIENT_SECRET,
+    #                              LINKEDIN_RETURN_URL)
+    #
+    # print(linkedin_auth.get_authorization_url())
+    # link = input("Enter Link:")
+    # auth_token = linkedin_auth.get_access_token_from_url(link)
+    #
+    linkedin_poster = LinkedIn(LINKEDIN_CLIENT_ID, LINKEDIN_CLIENT_SECRET,
+                               "AQVwWpDvP_2FTR5hvTHRXNbMKlWh5sK9BDVJmKG_fzAnKKgoT6bLDrZy0z3kN-RQa2e36evsfTMMlw4gq5BwlQiBbWUq7JIZAxALCssZ4nPPdeKFlEfnehh8Qqw87_eu6-ulyVPdWEsgS-zAxpNQPRW5IlJ2tQ56Lk_jGMlnnVowikarEfCs4VIqBo1GxP8YouYpefmwK_Yebelp-VFeRCpNJ3Nx1dAeLn3d5cRNyXE8nrvDYfqxSSQrtWewiNz1u9QUS-2wmOrLXjfJSfUuOM-stOi4Cbzcnrsh9xljamU9SKj-WjGQLFjfFEMqY-K3_-4aEAODYdzto-3rIAAzV6i6YInb4w")
     linkedin_poster.get_profile_name()
+    publish_update_company_page(message="Test Message", title="Title Message", company_id="13691092")
 
-    # linkedin_poster.publish_update("Only Update")
+    # linkedin_poster.publish_update("Only Updatesa")
     #
     # linkedin_poster.publish_update_with_attachment("Update with Attachment",
     #                                                "name_att", "link_att",
