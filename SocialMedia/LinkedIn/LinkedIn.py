5818ea42e2738775a10538771c5f38512b093bb2
Rohan Sawant <rohansawantct83@gmail.com>
2018-03-27 20:35:20 +0530

    Linkedin Company page posting, return post link

--- SocialMedia/LinkedIn/LinkedIn.py
+++ SocialMedia/LinkedIn/LinkedIn.py
@@ -43,39 +43,6 @@ class LinkedInAuth:
         return result.access_token
 
 
-def publish_update_company_page(message="", title="", company_id=""):
-    import requests
-    auth = "AQVwWpDvP_2FTR5hvTHRXNbMKlWh5sK9BDVJmKG_fzAnKKgoT6bLDrZy0z3kN-RQa2e36evsfTMMlw4gq5BwlQiBbWUq7JIZAxALCssZ4nPPdeKFlEfnehh8Qqw87_eu6-ulyVPdWEsgS-zAxpNQPRW5IlJ2tQ56Lk_jGMlnnVowikarEfCs4VIqBo1GxP8YouYpefmwK_Yebelp-VFeRCpNJ3Nx1dAeLn3d5cRNyXE8nrvDYfqxSSQrtWewiNz1u9QUS-2wmOrLXjfJSfUuOM-stOi4Cbzcnrsh9xljamU9SKj-WjGQLFjfFEMqY-K3_-4aEAODYdzto-3rIAAzV6i6YInb4w"
-
-    json_d = {
-        "visibility": {"code": "anyone"},
-        "comment": "Testing a full company share!",
-        "content": {
-            "submitted-­url": "https://www.example.com/content.html",
-            "title": "Test Share with Content",
-            "description": "content description",
-            "submitted‐image-­url": "https://www.example.com/img.jpg"
-        }
-    }
-    from flask import json
-    json_j = json.dumps(json_d)
-
-    url = "https://api.linkedin.com/v1/companies/{}" \
-          "/shares?oauth2_access_token={}" \
-          "&format=json".format(company_id, auth)
-    header = {'Content-Type': 'application/json',
-              "x-li-format": "json",
-              'Connection': 'Keep-Alive'
-              }
-    print("Headers:", header)
-    print("url:", url)
-    print("json_j:", json_j)
-
-    status = requests.post(url, headers=header, data=json_j)
-    print(str(status))
-    status = dict(status.json())
-
-
 class LinkedIn(SocialMedia):
     # TODO Fix Linkedin .submit_share(comment, description) confusion
     def __init__(self, client_id, client_secret, oauth=""):
@@ -121,6 +88,48 @@ class LinkedIn(SocialMedia):
         profile = self.linkedin_api.get_profile()
         return profile['firstName'] + " " + profile['lastName']
 
+    def publish_update_company_page(self, message="", title="", company_id="",
+                                    submitted_url="",
+                                    submitted_image_url=""):
+        import requests
+        from flask import json
+
+        json_d = {
+            "visibility": {"code": "anyone"},
+            "comment": message,
+        }
+        if submitted_url or submitted_image_url:
+            json_d["content"] = {
+                "submitted-­url": submitted_url,
+                "title": title,
+                "description": message,
+                "submitted‐image-­url": submitted_image_url
+            }
+
+        json_j = json.dumps(json_d)
+        auth = self.oauth_token.strip()
+        url = "https://api.linkedin.com/v1/companies/{}" \
+              "/shares?oauth2_access_token={}" \
+              "&format=json".format(company_id, auth)
+        header = {'Content-Type': 'application/json',
+                  "x-li-format": "json",
+                  'Connection': 'Keep-Alive'
+                  }
+        print("Auth Token:", auth)
+        print("Headers:", header)
+        print("url:", url)
+        print("json_j:", json_j)
+
+        status = requests.post(url, headers=header, data=json_j)
+        print(status.json())
+        status = dict(status.json())
+
+        url = "https://www.linkedin.com/feed/update/urn:li:activity:{}/" \
+            .format(status['updateKey'].split('-')[-1])
+
+        self.post_url = url
+        return self.post_url
+
 
 def main():
     # linkedin_auth = LinkedInAuth(LINKEDIN_CLIENT_ID,
@@ -133,8 +142,13 @@ def main():
     #
     linkedin_poster = LinkedIn(LINKEDIN_CLIENT_ID, LINKEDIN_CLIENT_SECRET,
                                "AQVwWpDvP_2FTR5hvTHRXNbMKlWh5sK9BDVJmKG_fzAnKKgoT6bLDrZy0z3kN-RQa2e36evsfTMMlw4gq5BwlQiBbWUq7JIZAxALCssZ4nPPdeKFlEfnehh8Qqw87_eu6-ulyVPdWEsgS-zAxpNQPRW5IlJ2tQ56Lk_jGMlnnVowikarEfCs4VIqBo1GxP8YouYpefmwK_Yebelp-VFeRCpNJ3Nx1dAeLn3d5cRNyXE8nrvDYfqxSSQrtWewiNz1u9QUS-2wmOrLXjfJSfUuOM-stOi4Cbzcnrsh9xljamU9SKj-WjGQLFjfFEMqY-K3_-4aEAODYdzto-3rIAAzV6i6YInb4w")
-    linkedin_poster.get_profile_name()
-    publish_update_company_page(message="Test Message", title="Title Message", company_id="13691092")
+    print(linkedin_poster.get_profile_name())
+    linkedin_poster.publish_update_company_page(message="Test23 Message",
+                                                title="Title Message",
+                                                company_id="13691092",
+                                                # submitted_url="https://www.google.com",
+                                                # submitted_image_url="https://example.com/example.jpg")
+                                                )
 
     # linkedin_poster.publish_update("Only Updatesa")
     #
